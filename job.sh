f1a1ee5c4ff524f47212495b129f80142ca1168d
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-06 12:06:06 +0200

    .

--- job.sh
+++ job.sh
@@ -1,30 +1,41 @@
 #!/usr/local_rwth/bin/zsh
 
-# COMMANDS to remember:
+### COMMANDS to remember:
 #  - shh login:     ssh -l ppxxxxxx login18-g-1.hpc.itc.rwth-aachen.de
 #  - submit job:    sbatch <script>.sh
 #  - list jobs:     sacct
 #  - list gpu's:    nvidia-smi
 
 
-# ask for 10 GB memory
+
+### CONFIGURATION
+
+# memory
 #SBATCH --mem-per-cpu=4G
-# name the job
+
+# job name
 #SBATCH --job-name=RubiksDL
+
 # declare the merged STDOUT/STDERR file
 #SBATCH --output=output/output.%J.txt
 
 # GPU
 #SBATCH --gres=gpu:1
 
-# time
+# max runing time
 #SBATCH --time=00:59:00
 
-### Change to the work directory
+
+### SCRIPT TO RUN
+
+# change to the work directory
 export PATH=$PATH:~/.local/bin
+# load python
 module load python/3.6.0
 
+# enter git repo folder
 cd ~/Rubiks-Cube-DL/
 
-### Execute your application
+# run file through pipenv
+# (makes sure dependencies are all there)
 pipenv run python train.py --ini ini/cube2x2-zero-goal-d200.ini -n run_${LSB_JOBID}
