8a81e702fb1c82c41eca5f7a14c958e2f43b0e4d
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-09 13:21:53 +0200

    .

--- solver.py
+++ solver.py
@@ -63,6 +63,7 @@ def gather_data(cube_env, net, max_seconds, max_steps, max_depth, samples_per_de
             solved_count = 0
 
             iterations_needed = []
+            solution_length = []
             for task_idx in tqdm(range(samples_per_depth)):
                 start_dt = datetime.datetime.utcnow()
                 task = generate_task(cube_env, depth)
@@ -83,12 +84,15 @@ def gather_data(cube_env, net, max_seconds, max_steps, max_depth, samples_per_de
                                        depth_max=tree_depth_stats['max'], depth_mean=tree_depth_stats['mean'])
                 result.append(data_point)
                 iterations_needed.append(len(tree))
+                iterations_needed.append(len(solution))
                 if is_solved:
                     solved_count += 1
             log.info("Depth %d processed, solved %d/%d (%.2f%%)", depth, solved_count, samples_per_depth,
                      100.0*solved_count/samples_per_depth)
-            # print('iterations per solve:', min(iterations_needed), sum(iterations_needed) / len(iterations_needed), max(iterations_needed))
+
+            np.set_printoptions(formatter={'float': '{: 0.1f}'.format})
             print('iterations per solve:', np.percentile(iterations_needed, [0, 25, 50, 75, 100]))
+            print('solution length:', np.percentile(iterations_needed, [0, 25, 50, 75, 100]))
     except KeyboardInterrupt:
         log.info("Interrupt received, got %d data samples, use them", len(result))
     return result
