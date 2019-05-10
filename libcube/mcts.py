8f2a89c508155a75ef85ff5a862978a10a9287cd
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-10 12:03:27 +0200

    .

--- libcube/mcts.py
+++ libcube/mcts.py
@@ -87,9 +87,10 @@ class Greedy:
             value, s, path = q.get()
             # seen.add(s)
             c_states, c_goals = self.cube_env.explore_state(s)
-            # values = self.eval_states_values(c_states)
-            policy, values = self.evaluate_states(c_states)
-            for a_idx, (probability, value, c_state, c_goal) in enumerate(zip(policy[0], values, c_states, c_goals)):
+            values = self.eval_states_values(c_states)
+            # policy, values = self.evaluate_states(c_states)
+            # for a_idx, (probability, value, c_state, c_goal) in enumerate(zip(policy[0], values, c_states, c_goals)):
+            for a_idx, (value, c_state, c_goal) in enumerate(zip(values, c_states, c_goals)):
                 self.nodes_evaluated += 1
 
                 p = path + [a_idx]
@@ -101,7 +102,7 @@ class Greedy:
                 # if c_state in seen:
                     # continue
 
-                heuristic = -value + len(p)# - probability
+                heuristic = -value + np.sqrt(len(p))# - probability
                 q.put((heuristic, c_state, p))
 
     def search(self):
