ee927dde6c68381e068d6479008094b38d5da33b
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-13 11:49:38 +0200

    more validation losses

--- train.py
+++ train.py
@@ -129,29 +129,36 @@ if __name__ == "__main__":
         if step_idx % config.validation_iters == 0:
             print(" ==== Validation ==== ")
 
-            solutions, iterations = validation.solve_random_cubes(cube_env,
-                scramble_depth=1,
-                amount=20,
-                max_iterations=200,
-                net=net, device=device)
+            def validation_stats(scramble_depth):
+                solutions, iterations = validation.solve_random_cubes(cube_env,
+                    scramble_depth=scramble_depth,
+                    amount=20,
+                    max_iterations=200,
+                    net=net, device=device)
 
-            np.set_printoptions(formatter={'float': '{: 0.1f}'.format})
 
-            solved = [s for s in solutions if s]
-            lengths = [len(s) for s in solved]
-            iterations_not_none = [i for i in iterations if i]
+                solved = [s for s in solutions if s]
+                lengths = [len(s) for s in solved]
+                iterations_not_none = [i for i in iterations if i]
 
-            print('iterations per solve:', np.percentile([i for i in iterations if i], [0, 25, 50, 75, 100]) if len(iterations_not_none) > 0 else None)
-            print('solution length:', np.percentile(lengths, [0, 25, 50, 75, 100]) if len(lengths) > 0 else None)
+                pct_solved = len([s for s in solved]) / len(solutions)
+                if len(iterations_not_none) > 0:
+                    iterations_75_percentile = np.percentile(iterations_not_none, [75])[0]
+                else:
+                    iterations_75_percentile = 0
 
-            pct_solved = len([s for s in solved]) / len(solutions)
-            if len(iterations_not_none) > 0:
-                iterations_75_percentile = np.percentile(iterations_not_none, [75])[0]
-            else:
-                iterations_75_percentile = 0
+                np.set_printoptions(formatter={'float': '{: 0.1f}'.format})
+                print('depth:', scramble_depth)
+                print('percent solved:', pct_solved)
+                print('iterations per solve:', np.percentile([i for i in iterations if i], [0, 25, 50, 75, 100]) if len(iterations_not_none) > 0 else None)
+                print('solution length:', np.percentile(lengths, [0, 25, 50, 75, 100]) if len(lengths) > 0 else None)
 
-            writer.add_scalar("validation_pct_solved", pct_solved, step_idx)
-            writer.add_scalar("iterations_75_percentile", iterations_75_percentile, step_idx)
+                writer.add_scalar(f"validation_pct_solved_{scramble_depth}", pct_solved, step_idx)
+                writer.add_scalar(f"iterations_75_percentile_{scramble_depth}", iterations_75_percentile, step_idx)
+
+            validation_stats(scramble_depth=3)
+            validation_stats(scramble_depth=7)
+            validation_stats(scramble_depth=20)
 
         if config.train_report_batches is not None and step_idx % config.train_report_batches == 0:
             m_policy_loss = np.mean(buf_policy_loss)
