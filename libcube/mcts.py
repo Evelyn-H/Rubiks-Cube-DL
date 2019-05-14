61e264d6e4590dd741f4bf1d95bb4f3bd5c561dc
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-14 17:52:01 +0200

    .

--- libcube/mcts.py
+++ libcube/mcts.py
@@ -119,8 +119,7 @@ class Greedy:
                     # print(iterations)
                     if len(p) > 1:
                         vals = self.eval_states_values(states)
-                        # print(f'values: {[round(v, 2) for v in vals[0]]}')
-                        print(f'values: {vals}')
+                        print(f'values: {[round(v[0], 2) for v in vals}')
                     return p
 
                 if len(p) > 40:
