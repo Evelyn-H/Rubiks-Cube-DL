ec99c241541be3d9a9fb3696bc84d6bce1b9e503
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-14 11:19:04 +0200

    trying something

--- libcube/mcts.py
+++ libcube/mcts.py
@@ -103,7 +103,9 @@ class Greedy:
                 # if c_state in seen:
                     # continue
 
-                heuristic = -value + len(p)# - probability
+                # heuristic = -value + len(p)# - probability
+                curve_val = -8.346825 + 8.494041 * np.exp(-0.1786749*len(p))
+                heuristic = -value + curve_val# - probability
                 q.put((heuristic, c_state, p))
 
     def search(self):
