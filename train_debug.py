4c939b948d11e8581579e9b792d48390c48e9dcf
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-16 12:03:33 +0200

    .

--- train_debug.py
+++ train_debug.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 
     # plot value per depth of scramble
     ideal_value = [-d for d in depths]
-    plot = sns.lineplot(depths, value)
+    plot = sns.lineplot(depths, value, ci=95)
     plot.plot(depths, ideal_value, scaley=False)
     plot.set_title("Values per depths")
     plot.get_figure().savefig(args.output + "-vals_vs_depths.png")
