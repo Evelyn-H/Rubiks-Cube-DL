46fb36ec92b7d769c9f10f8ecde7adddf6d836ee
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-20 11:38:57 +0200

    .

--- train_debug.py
+++ train_debug.py
@@ -86,7 +86,8 @@ if __name__ == "__main__":
     # plot value per depth of scramble
     optimal = optimal_lengths.all
     straight_line = [-d for d in depths]
-    plot = sns.lineplot(depths, value, ci="sd")
+    # plot = sns.lineplot(depths, value, ci="sd")
+    plot = sns.kdeplot(depths, value, shade=True)
     sns.lineplot([d[0] for d in optimal], [-d[1] for d in optimal], ci="sd", ax=plot)
     plot.plot(depths, straight_line, scaley=False)
     plot.set_title("Values per depths")
