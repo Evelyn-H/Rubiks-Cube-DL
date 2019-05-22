b71ac3230ce78e69409a6de5d36ab55810a56297
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-22 16:48:58 +0200

    .

--- job-optimal.sh
+++ job-optimal.sh
@@ -30,4 +30,4 @@ cd ~/Rubiks-Cube-DL/optimal
 # run file through pipenv
 # (makes sure dependencies are all there)
 
-optiqtm <scrambles-big-3.txt |tee results-big-3
+optiqtm <scrambles-big-4.txt |tee results-big-4
