e230001e5348c0737f6e1e370cc3889f3189217e
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-22 15:13:40 +0200

    .

--- train_debug.py
+++ train_debug.py
@@ -98,8 +98,8 @@ if __name__ == "__main__":
         optimal_per_dist[dist-1].append(-length)
     optimal_mean = np.array([sum(l) / len(l) for l in optimal_per_dist])
     optimal_errors = np.array([np.percentile(l, [0, 100]) for l in optimal_per_dist]).T
-    optimal_errors[0] -= optimal_mean
-    optimal_errors[1] -= optimal_mean
+    optimal_errors[0] = optimal_mean - optimal_errors[0]
+    optimal_errors[1] = optimal_errors[1] - optimal_mean
     print(optimal_mean)
     print(optimal_errors)
     plot.errorbar(range(1, MAX_DEPTH+1), optimal_mean, yerr=optimal_errors)
