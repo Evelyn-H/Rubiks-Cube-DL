52db2da48aa50d23a8ceb6fa4c3429cdae4bf7a8
Evelyn_H <hobert.evelyn@gmail.com>
2019-05-31 10:28:34 +0200

    .

--- libcube/mcts.py
+++ libcube/mcts.py
@@ -89,29 +89,29 @@ class Greedy:
             iterations += 1
             # if iterations > 10000:
             if iterations > self.max_iterations:
-                self.iterations_needed = iterations
-                l = [0] * 40
-                v = [[] for _ in range(40)]
-                o = 0
-                o_v = []
-
-                # print next best candidate state
-                value, s, path, states = q.get()
-                print(f'Best: ({value})')
-                self.cube_env.render(s)
-                q.put((value, s, path, states))
-                while not q.empty():
-                    value, s, path, states = q.get()
-                    if len(path)-1 < 40:
-                        l[len(path)-1] += 1
-                        v[len(path)-1].append(value)
-                    else:
-                        o += 1
-                        o_v.append(value)
-
-                print(*[f"{d}, {np.percentile(v, [0, 50, 100]) if len(v) > 0 else None}" for d, v in zip(l, v)], sep='\n')
-                # print(*[f"{d}" for d in l], sep='\n')
-                print('>40:', o)
+                # self.iterations_needed = iterations
+                # l = [0] * 40
+                # v = [[] for _ in range(40)]
+                # o = 0
+                # o_v = []
+                #
+                # # print next best candidate state
+                # value, s, path, states = q.get()
+                # print(f'Best: ({value})')
+                # self.cube_env.render(s)
+                # q.put((value, s, path, states))
+                # while not q.empty():
+                #     value, s, path, states = q.get()
+                #     if len(path)-1 < 40:
+                #         l[len(path)-1] += 1
+                #         v[len(path)-1].append(value)
+                #     else:
+                #         o += 1
+                #         o_v.append(value)
+                #
+                # print(*[f"{d}, {np.percentile(v, [0, 50, 100]) if len(v) > 0 else None}" for d, v in zip(l, v)], sep='\n')
+                # # print(*[f"{d}" for d in l], sep='\n')
+                # print('>40:', o)
                 return None
 
             value, s, path, states = q.get()
